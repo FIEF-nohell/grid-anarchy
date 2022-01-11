9c63c7ec68014e26d476662930446af455bea808
nohell <noel.hmnn@gmail.com>
2022-01-11 17:25:01 +0100

    painting works

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,12 +11,26 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+    this.reset()
+    
+  }
+
+  reset(){
     for(let i = 0; i < 900; i++){
       this.grid.push(0);
     }
-
-
-
   }
+
   
+  clicked(index: any, state: any){
+    console.log(state)
+    if(state == 0){
+      this.grid[index] = 1;
+    }
+    else if(state == 1){
+      this.grid[index] = 0;
+    }
+    console.log(this.grid)
+  }
+
 }
